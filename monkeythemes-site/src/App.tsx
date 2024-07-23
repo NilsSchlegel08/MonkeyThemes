ba5aa419d11390f6297fe9819bcb0d0dc36e13a0
Nils Schlegel <=>
2024-07-23 23:50:13 +0200

    MonkeyThemes Page setup.

---
+++ monkeythemes-site/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
+        <h1 className="text-4xl">This site is in work...</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
